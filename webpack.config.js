d9ea9884538c2c70c3aa6813766759b2e3635364
Rufin Hounkpe <rhounkpe@gmail.com>
2019-10-01 16:32:16 +0200

    inserting devServer config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/code.ts',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [ '.ts', '.js', '.tsx' ]
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: './src/code.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  }
 };
